b9114fc9b7e2fa7295b58cce753c13d180df2f46
Taweemeth <taweemeth.s@gmail.com>
2026-08-02 02:17:33 +0700

    try getByTestId

--- tests/smoke/homepage.spec.ts
+++ tests/smoke/homepage.spec.ts
@@ -7,6 +7,6 @@ test.describe('Toolshop homepage',() => {
     test('should display the product listing page', async ({page}) => {
         await expect(page).toHaveTitle(/Practice Software Testing/i);
 
-        await expect(page.locator('[data-test^="product-"]').first()).toBeVisible();    
+        await expect(page.getByTestId('product-name').first()).toBeVisible();    
     })
 }) 
